c864db8ee834f9e8cd3b9f469270ea851ba7ad09
Karol Olbryś  <karlson2002@gmail.com>
2024-03-17 18:21:37 +0100

    Pierwszy task

---
+++ main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in); 
+    System.out.println("Enter username");
+    int userInput = scan.nextInt();   
+
+    if(userInput > 0){
+        System.out.println("The number is positive");
+    }else if(userInput < 0){
+        System.out.println("The number is negative");
+    }else if (userInput == 0) {
+        System.out.println("The value equals 0");
+    }
+    }
+}
